d4a93447b7efe757eb996873be29812e17ab26f6
DevCircuss <98725465+DevCircuss@users.noreply.github.com>
2024-02-19 12:18:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = async () => {
+            await videoElement.play();
+            // Now that the video is playing, request Picture in Picture
+            await videoElement.requestPictureInPicture();
+        };
+    } catch (error) {
+        // Catch Error Here
+        console.log('error!', error);
+    }
+}
+
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    selectMediaStream();
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
